87ab4a176d8a89ec917f8706f06c45538a96b430
renaiku <renaiku.tv@gmail.com>
2019-02-01 23:47:27 +0100

    Working menu

--- js/main.js
+++ js/main.js
@@ -15,11 +15,27 @@ function changeDisplay(event, id, type) {
 
 }
 
-function getStyle(element, property)
-{
-	if (element.currentStyle)
-		var y = element.currentStyle[property];
-	else if (window.getComputedStyle)
-		var y = document.defaultView.getComputedStyle(element, null).getPropertyValue(property);
-	return y;
+function getStyle(element, property){
+
+	// Get the computed property of an element
+	// the if statement is here only for Internet Explorer
+	if (element.currentStyle) {
+		var computedProperty = element.currentStyle[property];
+	} else if (window.getComputedStyle) {
+		var computedProperty = document.defaultView.getComputedStyle(element, null).getPropertyValue(property);
+	}
+
+	return computedProperty;
+}
+
+window.onload = function () { 
+
+	// Get the selected menu item, and if it is a submenu item, open the category
+	var element = document.getElementById("current_menu_item");
+
+	if (element.classList.contains("js_identifier_submenu_item")) {
+		var parent = element.parentElement;
+		parent.style.display = "block";
+	}
+	
 }
