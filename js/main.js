70f8ef8199463a07507cda4f00533137c427d947
renaiku <corbeau.lenaic@gmail.com>
2019-02-01 15:39:27 +0100

    computedStyle for blank display value

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
 function changeDisplay(id, type) {
 
 	var element = document.getElementById(id);
+	var display = getStyle(element, "display");
 
-	if (element.style.display === "none") {
+	if (display === "none") {
 		element.style.display = type;
 	} else {
 		element.style.display = "none";
 	}
 
+}
+
+function getStyle(el, styleProp)
+{
+	var x = document.getElementById(el);
+	if (x.currentStyle)
+		var y = x.currentStyle[styleProp];
+	else if (window.getComputedStyle)
+		var y = document.defaultView.getComputedStyle(x,null).getPropertyValue(styleProp);
+	return y;
 }
